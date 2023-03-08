25c99a05aa44d017ee90e851c96f5e050117e6c0
Gajendrandev <727721euit038@skcet.ac.in>
2023-03-08 11:58:08 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import football from './football';
+// import Showdata from './Showdata';
+function App() {
+  return (
+    <div className="App">
+      <football/>
+      {/* <Showdata/>  */}
+    </div>
+  );
+}
+
+export default App;
